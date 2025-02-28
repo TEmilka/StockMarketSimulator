9c92f98ee37e90881f9eef2e176835f20f65ea9f
Emilia <emilkatrybala@gmail.com>
2025-03-01 00:24:25 +0100

    First commit

---
+++ backend/src/test/java/org/example/stockmarketsimulator/StockMarketSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stockmarketsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
