50c9a291a5e47225da9e02ad0522737e41d0d88c
Emilia <emilkatrybala@gmail.com>
2025-03-05 21:13:21 +0100

    Implement sample endpoint for [feature]

---
+++ backend/src/main/java/org/example/stockmarketsimulator/controller/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.stockmarketsimulator.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
